f7fb4564bbccc70bff6bde5de0404ab896b18bfb
bushbass <alex.nielsen@gmail.com>
2023-01-01 10:46:54 -0500

    corrected import of default list file

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,8 @@ import './App.css'
 import ListItem from './ListItem'
 import { v4 as uuidv4 } from 'uuid'
 import defaultItemsList from './defaultItemsList'
+
 function App() {
-  const defaultItemsList = [
-    {
-      input: 'cheap topsoil',
-      count: 1,
-      id: '836e3427-72d9-4666-a5d5-7d88323a32cf',
-    },
-    {
-      input: 'scotts topsoil',
-      count: 1,
-      id: 'deb4b8a9-3520-43a6-9e55-94ae1c6f3040',
-    },
-    {
-      input: 'scotts lawn soil',
-      count: 1,
-      id: '69e82fb2-d20c-42ab-9bd4-54f76dbb5445',
-    },
-    {
-      input: 'bovung',
-      count: 1,
-      id: 'be2b4f9c-4d45-4472-919a-b575e2b58851',
-    },
-    {
-      input: 'black cow',
-      count: 1,
-      id: '5b0b429c-439d-4810-bf46-8354760f1d6e',
-    },
-  ]
   const [input, setInput] = useState('')
   const [list, setList] = useState(() => {
     if (localStorage.hdList) {
--- src/defaultItemsList.js
+++ src/defaultItemsList.js
@@ -1,27 +1,27 @@
 const defaultItemsList = [
   {
     input: 'cheap topsoil',
-    count: 1,
+    count: 0,
     id: '836e3427-72d9-4666-a5d5-7d88323a32cf',
   },
   {
     input: 'scotts topsoil',
-    count: 1,
+    count: 0,
     id: 'deb4b8a9-3520-43a6-9e55-94ae1c6f3040',
   },
   {
     input: 'scotts lawn soil',
-    count: 1,
+    count: 0,
     id: '69e82fb2-d20c-42ab-9bd4-54f76dbb5445',
   },
   {
     input: 'bovung',
-    count: 1,
+    count: 0,
     id: 'be2b4f9c-4d45-4472-919a-b575e2b58851',
   },
   {
     input: 'black cow',
-    count: 1,
+    count: 0,
     id: '5b0b429c-439d-4810-bf46-8354760f1d6e',
   },
 ]
