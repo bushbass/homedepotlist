e01c56bfe2de277733599e3d9329a3edcfebf9fd
Alex Nielsen <alex.nielsen@gmail.com>
2023-02-15 02:44:02 -0500

    updated default list

--- src/defaultItemsList.js
+++ src/defaultItemsList.js
@@ -1,229 +1,254 @@
 const defaultItemsList = [
-    {
-        "input": "cheap topsoil",
-        "count": 0,
-        "id": "836e3427-72d9-4666-a5d5-7d88323a32cf"
-    },
-    {
-        "input": "scotts topsoil",
-        "count": 0,
-        "id": "deb4b8a9-3520-43a6-9e55-94ae1c6f3040"
-    },
-    {
-        "input": "scotts lawn soil",
-        "count": 0,
-        "id": "69e82fb2-d20c-42ab-9bd4-54f76dbb5445"
-    },
-    {
-        "input": "bovung",
-        "count": 0,
-        "id": "be2b4f9c-4d45-4472-919a-b575e2b58851"
-    },
-    {
-        "input": "black cow",
-        "count": 0,
-        "id": "5b0b429c-439d-4810-bf46-8354760f1d6e"
-    },
-    
-    {
-        "input": "nat. care garden",
-        "count": 0,
-        "id": "7942f00a-347a-4e56-9c17-48929d233e0b"
-    },
-    {
-        "input": "nat. care compost",
-        "count": 0,
-        "id": "10da0cc0-60e3-4a25-a575-cee9cf1a826b"
-    },
-    {
-        "input": "MG garden sm",
-        "count": 0,
-        "id": "90d8cbdb-4957-43e9-8c43-bcc9e7a830f7"
-    },
-    {
-        "input": "MG garden lg",
-        "count": 0,
-        "id": "6584bd81-534f-4886-a3f7-1201aeb96891"
-    },
-    {
-        "input": "vigoro garden 1 foot",
-        "count": 0,
-        "id": "d6be08e8-5f5e-4010-a4e5-279a774662dd"
-    },
-    {
-        "input": "MG garden purple",
-        "count": 0,
-        "id": "a399954f-4c65-48cc-97e9-30ce589fac5a"
-    },
-    {
-        "input": "MG tree and shrub",
-        "count": 0,
-        "id": "3d41a2bb-aa19-4207-921e-7093167c9881"
-    },
-    {
-        "input": "MG garden white",
-        "count": 0,
-        "id": "12a601e6-eed2-4ef5-b3e2-99f8611096c8"
-    },
-    {
-        "input": "MG raised bed",
-        "count": 0,
-        "id": "3f3788db-17e0-4fd3-9c95-ac0a0b12efd6"
-    },
-    {
-        "input": "Bonnie raised bed",
-        "count": 0,
-        "id": "1a4f02bb-7e2c-4785-ac7e-1b975afb9db6"
-    },
-    {
-        "input": "nat. care organic pot",
-        "count": 0,
-        "id": "a454065a-c8a9-4a90-ad98-63f073639ec6"
-    },
-    {
-        "input": "vigoro pot blue 32qt",
-        "count": 0,
-        "id": "cce7d84d-946f-42ca-8ac3-a20e9af0be42"
-    },
-    {
-        "input": "MG indoor pot",
-        "count": 0,
-        "id": "328d345d-d5fc-4fda-9803-84f19cb130fe"
-    },
-    {
-        "input": "MG pot yel lg",
-        "count": 0,
-        "id": "be82dc99-1374-4bbf-b969-0394b6afc0da"
-    },
-    {
-        "input": "MG pot yel sm",
-        "count": 0,
-        "id": "c4c1e2b3-9b40-490c-9e3d-c03830006154"
-    },
-    {
-        "input": "MG pot blue lg",
-        "count": 0,
-        "id": "19bebd46-66a8-4577-82b6-fbe205c82ab1"
-    },
-    {
-        "input": "MG pot blue sm",
-        "count": 0,
-        "id": "9160d7e8-db81-4382-b72d-2f5cb8341cea"
-    },
-    {
-        "input": "paver base",
-        "count": 0,
-        "id": "93daeb48-c623-4251-9079-a6b57b6b8d9b"
-    },
-    {
-        "input": "leveling sand",
-        "count": 0,
-        "id": "021bdd8e-5d11-49f3-a2f6-53b115d7007e"
-    },
-    {
-        "input": "Permasand gray",
-        "count": 0,
-        "id": "3160680c-44b2-4c9d-8353-c2b7b78948a8"
-    },
-    {
-        "input": "Permasand tan",
-        "count": 0,
-        "id": "60437ced-7060-40dc-96fd-9ae3b9c7d3fd"
-    },
-    {
-        "input": "mini-marble",
-        "count": 0,
-        "id": "d661fe9b-ec0c-4e06-aeff-d7354fe347cb"
-    },
-    {
-        "input": "play sand",
-        "count": 0,
-        "id": "6ea9511d-0495-464d-9260-48ef62c95926"
-    },
-    {
-        "input": "all purpose Stone",
-        "count": 0,
-        "id": "918945ec-4b34-4f30-a68b-55cb6c88acbb"
-    },
-    {
-        "input": "Vista red",
-        "count": 0,
-        "id": "08e606b8-2651-487f-a545-0c6f34660e6b"
-    },
-    {
-        "input": "marble chips",
-        "count": 0,
-        "id": "52afc9d2-9182-4b82-8005-38e06f6b4ad6"
-    },
-    {
-        "input": "pond pebbles",
-        "count": 0,
-        "id": "16b1a150-a7fe-428f-803f-04b14744eda7"
-    },
-    {
-        "input": "pea pebbles",
-        "count": 0,
-        "id": "2f106445-7dd7-42d6-9526-d62bebc27645"
-    },
-    {
-        "input": "River pebbles",
-        "count": 0,
-        "id": "c1ceff04-91c5-4adf-84e0-f0a240f62841"
-    },
-    {
-        "input": "Creek stone",
-        "count": 0,
-        "id": "c5c60232-a050-4ec8-8af6-9fc405736d6d"
-    },
-    {
-        "input": "pine bark nuggets",
-        "count": 0,
-        "id": "9613d331-9936-455a-80fe-dea7ad63c769"
-    },
-    {
-        "input": "cedar mulch",
-        "count": 0,
-        "id": "3331427b-21f1-4909-832a-67ea452b9b04"
-    },
-    {
-        "input": "vigoro red",
-        "count": 0,
-        "id": "f17f48de-8ea4-4a27-b563-960f8d9b807d"
-    },
-    {
-        "input": "vigoro Brown",
-        "count": 0,
-        "id": "cbf23580-ea7a-4416-b243-54543367db94"
-    },
-    {
-        "input": "vigoro black",
-        "count": 0,
-        "id": "53155dd7-efd3-400c-ac9f-f2fb4206f6b8"
-    },
-    {
-        "input": "earthgro Brown",
-        "count": 0,
-        "id": "ffa1634c-7f0e-4103-bff0-17e20dda1f29"
-    },
-    {
-        "input": "earthgro black",
-        "count": 0,
-        "id": "700f9b7e-3956-4b94-904a-d5f5d4921492"
-    },
-    {
-        "input": "earthgro red",
-        "count": 0,
-        "id": "d27bbb59-6c76-4de9-bd98-d3a79e7dd0e8"
-    },
-    {
-        "input": "hay bales",
-        "count": 0,
-        "id": "5bd10187-567a-4e76-8bcc-922606d2107a"
-    },
-    {
-        "input": "Pelletized lime",
-        "count": 0,
-        "id": "06dc2868-7572-4020-b611-2bce90c9e48e"
-    }
-]
-export default defaultItemsList
+  {
+    input: 'cheap topsoil',
+    count: 0,
+    id: '836e3427-72d9-4666-a5d5-7d88323a32cf',
+  },
+  {
+    input: 'black cow',
+    count: 0,
+    id: '5b0b429c-439d-4810-bf46-8354760f1d6e',
+  },
+  {
+    input: 'scotts topsoil',
+    count: 0,
+    id: 'deb4b8a9-3520-43a6-9e55-94ae1c6f3040',
+  },
+  {
+    input: 'scotts lawn soil',
+    count: 0,
+    id: '69e82fb2-d20c-42ab-9bd4-54f76dbb5445',
+  },
+  {
+    input: 'bovung',
+    count: 0,
+    id: 'be2b4f9c-4d45-4472-919a-b575e2b58851',
+  },
+  {
+    input: 'black cow garden',
+    count: 1,
+    id: 'e67cb163-dda9-4557-ac5b-4d7caf5c69e1',
+  },
+  {
+    input: 'kellog garden',
+    count: 1,
+    id: '94840e49-756d-4426-9528-00bc1b7fb678',
+  },
+  {
+    input: 'nat. care garden',
+    count: 0,
+    id: '7942f00a-347a-4e56-9c17-48929d233e0b',
+  },
+  {
+    input: 'nat. care compost',
+    count: 0,
+    id: '10da0cc0-60e3-4a25-a575-cee9cf1a826b',
+  },
+  {
+    input: 'vigoro garden 1 foot',
+    count: 0,
+    id: 'd6be08e8-5f5e-4010-a4e5-279a774662dd',
+  },
+  {
+    input: 'MG garden sm',
+    count: 0,
+    id: '90d8cbdb-4957-43e9-8c43-bcc9e7a830f7',
+  },
+  {
+    input: 'MG garden lg',
+    count: 0,
+    id: '6584bd81-534f-4886-a3f7-1201aeb96891',
+  },
+  {
+    input: 'MG garden purple',
+    count: 0,
+    id: 'a399954f-4c65-48cc-97e9-30ce589fac5a',
+  },
+  {
+    input: 'black cow raised bed',
+    count: 1,
+    id: 'eb4a0207-1bd8-4559-b1a3-3588d7e2375f',
+  },
+  {
+    input: 'kellog raised bed',
+    count: 1,
+    id: 'a6aebf96-512c-4083-be88-ef26fcdaeecb',
+  },
+  {
+    input: 'MG tree and shrub',
+    count: 0,
+    id: '3d41a2bb-aa19-4207-921e-7093167c9881',
+  },
+  {
+    input: 'MG garden white',
+    count: 0,
+    id: '12a601e6-eed2-4ef5-b3e2-99f8611096c8',
+  },
+  {
+    input: 'MG raised bed',
+    count: 0,
+    id: '3f3788db-17e0-4fd3-9c95-ac0a0b12efd6',
+  },
+  {
+    input: 'Bonnie raised bed',
+    count: 0,
+    id: '1a4f02bb-7e2c-4785-ac7e-1b975afb9db6',
+  },
+  {
+    input: 'black cow pot',
+    count: 1,
+    id: '2d0e1401-360d-4fef-8850-2414c732a653',
+  },
+  { input: 'kellog pot', count: 1, id: 'def1d4cc-05c3-4cf9-82bf-6f0439774e7b' },
+  {
+    input: 'nat. care organic pot',
+    count: 0,
+    id: 'a454065a-c8a9-4a90-ad98-63f073639ec6',
+  },
+  {
+    input: 'vigoro pot blue 32qt',
+    count: 0,
+    id: 'cce7d84d-946f-42ca-8ac3-a20e9af0be42',
+  },
+  {
+    input: 'MG indoor pot',
+    count: 0,
+    id: '328d345d-d5fc-4fda-9803-84f19cb130fe',
+  },
+  {
+    input: 'MG pot yel lg',
+    count: 0,
+    id: 'be82dc99-1374-4bbf-b969-0394b6afc0da',
+  },
+  {
+    input: 'MG pot yel sm',
+    count: 0,
+    id: 'c4c1e2b3-9b40-490c-9e3d-c03830006154',
+  },
+  {
+    input: 'MG pot blue lg',
+    count: 0,
+    id: '19bebd46-66a8-4577-82b6-fbe205c82ab1',
+  },
+  {
+    input: 'MG pot blue sm',
+    count: 0,
+    id: '9160d7e8-db81-4382-b72d-2f5cb8341cea',
+  },
+  {
+    input: 'paver base',
+    count: 0,
+    id: '93daeb48-c623-4251-9079-a6b57b6b8d9b',
+  },
+  {
+    input: 'leveling sand',
+    count: 0,
+    id: '021bdd8e-5d11-49f3-a2f6-53b115d7007e',
+  },
+  {
+    input: 'Permasand gray',
+    count: 0,
+    id: '3160680c-44b2-4c9d-8353-c2b7b78948a8',
+  },
+  {
+    input: 'Permasand tan',
+    count: 0,
+    id: '60437ced-7060-40dc-96fd-9ae3b9c7d3fd',
+  },
+  {
+    input: 'mini-marble',
+    count: 0,
+    id: 'd661fe9b-ec0c-4e06-aeff-d7354fe347cb',
+  },
+  {
+    input: 'play sand',
+    count: 0,
+    id: '6ea9511d-0495-464d-9260-48ef62c95926',
+  },
+  {
+    input: 'all purpose Stone',
+    count: 0,
+    id: '918945ec-4b34-4f30-a68b-55cb6c88acbb',
+  },
+  {
+    input: 'Vista red',
+    count: 0,
+    id: '08e606b8-2651-487f-a545-0c6f34660e6b',
+  },
+  {
+    input: 'marble chips',
+    count: 0,
+    id: '52afc9d2-9182-4b82-8005-38e06f6b4ad6',
+  },
+  {
+    input: 'pond pebbles',
+    count: 0,
+    id: '16b1a150-a7fe-428f-803f-04b14744eda7',
+  },
+  {
+    input: 'pea pebbles',
+    count: 0,
+    id: '2f106445-7dd7-42d6-9526-d62bebc27645',
+  },
+  {
+    input: 'River pebbles',
+    count: 0,
+    id: 'c1ceff04-91c5-4adf-84e0-f0a240f62841',
+  },
+  {
+    input: 'Creek stone',
+    count: 0,
+    id: 'c5c60232-a050-4ec8-8af6-9fc405736d6d',
+  },
+  {
+    input: 'pine bark nuggets',
+    count: 0,
+    id: '9613d331-9936-455a-80fe-dea7ad63c769',
+  },
+  {
+    input: 'cedar mulch',
+    count: 0,
+    id: '3331427b-21f1-4909-832a-67ea452b9b04',
+  },
+  {
+    input: 'vigoro red',
+    count: 0,
+    id: 'f17f48de-8ea4-4a27-b563-960f8d9b807d',
+  },
+  {
+    input: 'vigoro Brown',
+    count: 0,
+    id: 'cbf23580-ea7a-4416-b243-54543367db94',
+  },
+  {
+    input: 'vigoro black',
+    count: 0,
+    id: '53155dd7-efd3-400c-ac9f-f2fb4206f6b8',
+  },
+  {
+    input: 'earthgro Brown',
+    count: 0,
+    id: 'ffa1634c-7f0e-4103-bff0-17e20dda1f29',
+  },
+  {
+    input: 'earthgro black',
+    count: 0,
+    id: '700f9b7e-3956-4b94-904a-d5f5d4921492',
+  },
+  {
+    input: 'earthgro red',
+    count: 0,
+    id: 'd27bbb59-6c76-4de9-bd98-d3a79e7dd0e8',
+  },
+  {
+    input: 'hay bales',
+    count: 0,
+    id: '5bd10187-567a-4e76-8bcc-922606d2107a',
+  },
+  {
+    input: 'Pelletized lime',
+    count: 0,
+    id: '06dc2868-7572-4020-b611-2bce90c9e48e',
+  },
+];
+export default defaultItemsList;
